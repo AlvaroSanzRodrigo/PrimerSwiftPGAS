ad63bf2e761d8d472fa0706cf62ee35e6c7c9036
Alvaro Sanz Rodrigo <alvaro.sanz@live.u-tad.com>
2018-03-15 04:23:29 -0700

    Innit Commit

---
+++ PrimerProyectoYony/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrimerProyectoYony
+//
+//  Created by Álvaro Sanz Rodrigo on 15/3/18.
+//  Copyright © 2018 SanzAndCompanyCorp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
